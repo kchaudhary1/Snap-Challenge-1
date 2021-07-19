81b904d0a23cc75c0faa9732217521d13f74c563
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-07-19 10:17:03 -0600

    Added more functions answers.

--- Functions/more-functions-1.js
+++ Functions/more-functions-1.js
@@ -27,3 +27,20 @@ const smallBagOfCoffeeWeight = convertToGrams(1, 'Nt')
 console.log(smallBagOfCoffeeWeight)
 const waterInCup = convertToGrams(12, 'oz')
 console.log(waterInCup)
+
+// 2.Write a function in JavaScript that takes a number and finds the sum of all unique positive factors of a number. (The factors of a number are all numbers that divide evenly into it. For example, the unique positive factors of 12 are 1, 2, 3, 4, 6, and 12, and the sum would thus be 28.)
+
+const factor = function (number) {
+    let sum = 0
+    for (let i = 1; i <= number; i++){
+        if (number % i === 0) {
+          sum = sum + i
+        }
+    }
+
+    return sum
+}
+console.log(factor(13))
+// const fatArrowFactor = number => {}) -- does same thing as above used to declare function.
+
+
