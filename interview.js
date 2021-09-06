1bc51e90b128932e504ea3c0e3eb26b78a585af0
Komal Chaudhary <kchaudhary1@cnm.edu>
2021-09-06 08:41:51 -0600

    added answer of the question

---
+++ interview.js
@@ -0,0 +1,9 @@
+let array = []
+function newArray(array) {
+    for (i=0, i>=array.length i++)
+        let result = []
+        If (include.arrayresult[]) {
+        result+ = array[i]
+    }
+    return result
+}
